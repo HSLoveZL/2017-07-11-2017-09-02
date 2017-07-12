ae479af69d11415b99717b99acccd41dad761e51
HSLoveZL <hjmrezl@outlook.com>
2017-07-12 14:45:51 +0800

    add new dirs

---
+++ Unity5.6.0f3Gaze/Assets/Scripts/Gaze.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gaze : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = new Ray(transform.position, transform.forward);
+		RaycastHit hit;
+
+	}
+}
---
+++ testVRSamples/Assets/_Scripts/GazeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GazeController : MonoBehaviour {
+
+	/// <summary>
+	/// 准星容器
+	/// </summary>
+	public Canvas reticleCanvas;
+	/// <summary>
+	/// 准星图片
+	/// </summary>
+	public Image reticleImage;
+	/// <summary>
+	/// 击中的物体
+	/// </summary>
+	private GameObject target;
+	/// <summary>
+	/// 初始准星位置
+	/// </summary>
+	private Vector3 originPos;
+
+	// Use this for initialization
+	void Start () {
+		originPos = reticleCanvas.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = new Ray(transform.position, transform.forward);
+		RaycastHit hit;
+		if(Physics.Raycast(ray, out hit, 100))
+		{
+			reticleCanvas.transform.position = hit.point;
+
+		}
+		else
+		{
+			reticleCanvas.transform.localPosition = originPos;
+		}
+	}
+}
